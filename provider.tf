6dee7962b49620c1dd56739fda662c5d98305c14
sunitha2712 <sunitham351@gmail.com>
2026-07-16 20:15:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
  # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "newkops8"
+    bucket = "sunitha16"
     key    = "devops/terraform.tfstate"
     region = "ap-south-1"   
   }
